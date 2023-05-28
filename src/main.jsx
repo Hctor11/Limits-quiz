a0537a41c0c8481fd216a234fffc0f533b5ceb71
hector <hectorrrivera1010@gmail.com>
2023-05-28 17:07:23 -0600

    trying deployyy

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
+import Main from './Components/Main/Main.jsx'
+import Quiz from './Components/Quiz/Quiz.jsx'
+import FinishedQuiz from './Components/FinishedQuiz/FinishedQuiz.jsx'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Main/> 
+  },
+  {
+    path: '/quiz',
+    element: <Quiz/>
+  },
+  {
+    path: '/finished',
+    element: <FinishedQuiz/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router}>
     <App />
+    </RouterProvider>
   </React.StrictMode>,
 )
